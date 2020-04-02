b289232a2b3d0b754f8d9532e8e4ef07da47555e
Konstantin Mayev <k.mayev@gmail.com>
2020-04-02 21:56:20 +0300

    show method created

---
+++ src/view/ConsoleView.java
@@ -0,0 +1,42 @@
+package view;
+
+import io.hexlet.xo.model.Field;
+import io.hexlet.xo.model.Figure;
+import io.hexlet.xo.model.Game;
+import io.hexlet.xo.model.exceptions.InvalidPointException;
+
+import java.awt.*;
+
+public class ConsoleView {
+    public void show(final Game game){
+        System.out.format("Game name=%S\n",game.getName());
+        final Field field = game.getField();
+        for (int x =0; x < field.getSize(); x++) {
+            printline(field, x);
+        }
+        printSeparator();
+    }
+
+    public void move(final Game game){
+
+    }
+    private void printline(final Field field, final int x) {
+        Figure figure = null;
+     //   figure = field.getFigure(new Point(x, y));
+        printSeparator();
+        System.out.print("|");
+        for (int y = 0; y < field.getSize(); y++) {
+            try {
+                figure = field.getFigure(new Point(x, y));
+            } catch (InvalidPointException e) {
+                e.printStackTrace();
+            }
+                System.out.print(figure != null ? figure : " ");
+                System.out.print('|');
+        }
+        System.out.println();
+    }
+    private void printSeparator() {
+        System.out.println("~~~~~~~~~~~~~~~~");
+    }
+}
