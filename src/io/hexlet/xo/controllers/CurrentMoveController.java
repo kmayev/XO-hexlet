0b23bff77a9bae203024c5afb9770780317975fd
Konstantin Mayev <k.mayev@gmail.com>
2020-04-02 20:26:50 +0300

    CurrenMoveController - created and tests issued

---
+++ src/io/hexlet/xo/controllers/CurrentMoveController.java
@@ -0,0 +1,25 @@
+package io.hexlet.xo.controllers;
+
+import io.hexlet.xo.model.Field;
+import io.hexlet.xo.model.Figure;
+import io.hexlet.xo.model.exceptions.InvalidPointException;
+
+import java.awt.*;
+
+public class CurrentMoveController {
+    public Figure currentMove(final Field field){
+       int countFigure = 0;
+       for (int x = 0; x < field.getSize(); x++) {
+           for (int y = 0; y < field.getSize(); y++) {
+               try {
+                   if (field.getFigure(new Point(x, y)) != null) countFigure++;
+               } catch (InvalidPointException e) {
+                   e.printStackTrace();
+               }
+           }
+       }
+       if  (countFigure == field.getSize()*field.getSize()) return null;
+       if (countFigure%2 == 0) return Figure.X;
+       return Figure.O;
+    }
+}
